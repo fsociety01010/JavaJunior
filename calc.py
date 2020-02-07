d7ab6102f1ebcf5e086484c9c6279e448f9a1d0f
Alex <agarkovalexandre@gmail.com>
2020-02-07 13:54:59 +0100

    update

---
+++ calc.py
@@ -0,0 +1,5 @@
+def graphique(CT,CC,CP):
+    print((0.3* CP) + (0.7 * max(CT, (CC+CT) / 2)))
+
+graphique(9.6,5.42,11)
+## Graphique - min 9.6 point
---
+++ test.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+    for(int i = 0; i < 10; i+_)
+    int x = 10;
+    System.out.println(x);
+}
