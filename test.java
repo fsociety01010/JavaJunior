dba429730da1a0f6425be0f54249067bad435e03
fsociety01010 <agarkovalexandre@gmail.com>
2020-04-05 11:38:19 +0200

    fix

--- test.java
+++ test.java
@@ -2,16 +2,9 @@ import java.util.UUID;
 
 public class test {
     public static void main(String[] args) {
-        final String uuid = UUID.randomUUID().toString().replace("-", "");
-        System.out.println("uuid = " + uuid);
-
-        System.out.format("%,d \n",Long.MAX_VALUE);
-        System.out.format("%,d\n",Integer.MAX_VALUE);
-        System.out.format("%,f\n",Double.MAX_VALUE);
-        System.out.format("Long contains %,d integers\n", Long.MAX_VALUE / Integer.MAX_VALUE);
-        System.out.format("Double contains %,f longs\n", Double.MAX_VALUE / Long.MAX_VALUE);
-
-       
+      
+        int a = 123451231;
+      System.out.println("abc"  );
     }
 
 }
